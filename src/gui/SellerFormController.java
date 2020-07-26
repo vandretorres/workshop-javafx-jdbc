bba48ee686580a59c22c6f60d9c4ac0ed2276f81
vandretorres <vandre.torres@gmail.com>
2020-07-26 18:55:33 -0300

    Date Picker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,180 +20,150 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
 import model.exceptions.ValidationException;
 import model.services.SellerService;
 
-
 public class SellerFormController implements Initializable {
 
-
 	private Seller entity;
-
+	
 	private SellerService service;
-
+	
 	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
-
+	
 	@FXML
 	private TextField txtName;
-
+	
+	@FXML
+	private TextField txtEmail;
+	
+	@FXML
+	private DatePicker dpBirthDate;
+	
+	@FXML
+	private TextField txtBaseSalary;
+	
 	@FXML
 	private Label labelErrorName;
-
+	
+	@FXML
+	private Label labelErrorEmail;
+	
+	@FXML
+	private Label labelErrorBirthDate;
+	
+	@FXML
+	private Label labelErrorBaseSalary;
+	
 	@FXML
 	private Button btSave;
-
+	
 	@FXML
 	private Button btCancel;
-
-
-	//Action event vai pegar o evento da janela e permite controlar ela. usado no comando de fechar a janela
-	@FXML 
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	@FXML
 	public void onBtSaveAction(ActionEvent event) {
-
 		if (entity == null) {
 			throw new IllegalStateException("Entity was null");
 		}
-
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Service was null");
 		}
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
-			notifyDataChangeListener();
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
-			
-		}catch (DbException e) {
-			Alerts.showAlert("Error Saving Objecto", null, e.getMessage(), AlertType.ERROR);
-			
-		}catch (ValidationException e) {
+		}
+		catch (ValidationException e) {
 			setErrorMessages(e.getErrors());
 		}
-
-
-
-	}
-
-	
-	//metodo que emite o evento para outras classes
-	private void notifyDataChangeListener() {
-		for (DataChangeListener listener : dataChangeListeners) {			
-			listener.onDataChange();			
-		}	
-	}
-	
-
-	@FXML 
-	public void onBtCancelAction(ActionEvent event) {
-
-		Utils.currentStage(event).close();
-	}
-
-
-
-
-	@Override
-	public void initialize(URL uri, ResourceBundle rb) {
-
-		InitializeNodes();
-
-	}
-
-	private void InitializeNodes() {
-
-		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
-
-	}
-
-
-	//seta objeto department no atributo entity ( objeto vem do SellerListController )
-	public void setSeller(Seller entity) {
-		this.entity = entity;
-	}
-
-
-	// atualiza os campos TXT com os valores do objeto
-	public void updateFormData() {
-
-		if ( entity == null) {
-			throw new IllegalStateException("Entity was null");
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
-
-		txtId.setText(String.valueOf(entity.getId()));
-
-		txtName.setText(entity.getName());
-
 	}
 	
-	
-    // Injeta objeto SellerService no Atributo Service .. chamado pelo DeparmentListController
-	public void setSellerService(SellerService service) {
-		this.service = service;
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChange();
+		}
 	}
 
-	
-	
-	// Cria um objeto Deparment com os dados informados pelo usuário
 	private Seller getFormData() {
-
-		//Cria objeto ValidationException lançando mensagem genérica para classe superior RunTimeException
-		ValidationException exception = new ValidationException("Validation error");		
-		
 		Seller obj = new Seller();
-		obj.setId(Utils.tryParsetoInt(txtId.getText()));			
 		
+		ValidationException exception = new ValidationException("Validation error");
 		
-		/*
-		 * Comandos abaixo irão validar se campo Name do form Seller está vazio
-		 * Caso esteja vazio o método addError irá adicionar a chave com o nome do campo e a descrição do erro
-		 * 
-		 * Em seguida se a Lista Map dot ipo Exception possui registro para poder lançar a exceção parao  método que fez a chamda do getFormData ( onButtonSaveActiion)
-		 * 
-		 */
-		if ( txtName.getText() == null || txtName.getText().trim().equals("")) {
+		obj.setId(Utils.tryParsetoInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can't be empty");
 		}
 		obj.setName(txtName.getText());
 		
-		if ( exception.getErrors().size() > 0) {
+		if (exception.getErrors().size() > 0) {
 			throw exception;
-		}		
-		return obj;	
-	}
-	
-	
-	// Este método irá inscrever os listeners na lista
-	//Qualquer classe que queira sre notificado da altreação da tabela Seller deverá impelmentar a interface
-	//DatachangeListener
-	public void subscribeDataChangeListener(DataChangeListener listener) {
-		
-		dataChangeListeners.add(listener);
-		
-	}
-	
-	
-	/*
-	 * Método lança a exceção na label labelErrorName
-	 * comando Set irá armazenar as chaves da lista Map	 
-	 */
-	private void setErrorMessages(Map<String , String> errors) {
-		
-		Set<String> fields = errors.keySet();
-		
-		if(fields.contains("name")){
-			
-			labelErrorName.setText(errors.get("name"));
 		}
 		
-		
+		return obj;
 	}
 
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
 	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 70);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if (entity.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
 }
